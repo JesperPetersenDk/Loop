bc0e36716d726a34a1846d0bc9be3aba5c622d47
Jesper Petersen <info@ordklar.dk>
2018-04-24 19:43:41 +0200

    Loops

---
+++ src/loop/Loop.java
@@ -0,0 +1,23 @@
+
+package loop;
+
+public class Loop {
+
+    public static void main(String[] args) {
+        
+        int kVal = 8;
+        int kv = 1;
+        
+        //While loop
+        while(kVal > 1)
+        {
+            kv *= kVal;
+            kVal -= 1;
+        }
+        System.out.println(kv);
+        
+        //do-while loop
+        
+    }
+    
+}
